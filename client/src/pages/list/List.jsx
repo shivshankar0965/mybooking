f4c27de326a53066bbb422a04c4cd7bb744ccfff
Shiv Shankar <shivshankar0965@gmail.com>
2023-06-11 17:07:51 +0530

    added search hotels more options

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -8,6 +8,7 @@ import { DateRange } from "react-date-range";
 const List = () => {
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
+  const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState(location.state.date);
   const [options, setOptions] = useState(location.state.options);
 
@@ -25,16 +26,57 @@ const List = () => {
             </div>
             <div className="ls_item">
               <label>Check-in date</label>
-              <span>{`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(
-                date[0].endDate,
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                date[0].startDate,
                 "dd/MM/yyyy"
-              )}`}</span>
-              <DateRange
-                onChange={(item) => setDate([item.selection])}
-                minDate={new Date()}
-                moveRangeOnFirstSelection={false}
-                ranges={date}
-              />
+              )} to ${format(date[0].endDate, "dd/MM/yyyy")}`}</span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => setDate([item.selection])}
+                  minDate={new Date()}
+                  moveRangeOnFirstSelection={false}
+                  ranges={date}
+                />
+              )}
+            </div>
+            <div className="ls_item">
+              <label>Options</label>
+              <div className="ls_option_item">
+                <span className="ls_option_text">
+                  Min Price <small>per night</small>
+                </span>
+                <input type="number" className="ls_option_input" />
+              </div>
+              <div className="ls_option_item">
+                <span className="ls_option_text">
+                  Max Price <small>per night</small>
+                </span>
+                <input type="number" className="ls_option_input" />
+              </div>
+              <div className="ls_option_item">
+                <span className="ls_option_text">Adult</span>
+                <input
+                  type="number"
+                  className="ls_option_input"
+                  placeholder={options.adult}
+                />
+              </div>
+              <div className="ls_option_item">
+                <span className="ls_option_text">Children</span>
+                <input
+                  type="number"
+                  className="ls_option_input"
+                  placeholder={options.child}
+                />
+              </div>
+              <div className="ls_option_item">
+                <span className="ls_option_text">Room</span>
+                <input
+                  type="number"
+                  className="ls_option_input"
+                  placeholder={options.room}
+                />
+              </div>
             </div>
           </div>
           <div className="list_result"></div>
